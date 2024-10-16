29149aa543b941a6b813b3e00cb0fc4216982817
dexterbakr <bakrmostafa20@gmail.com>
2024-10-16 06:34:17 +0300

    yarab

--- src/app/component/create-an-event/create-an-event.component.spec.ts
+++ src/app/component/create-an-event/create-an-event.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { USERMODEL } from './userModel.js';
+import { USERMODEL } from './usermodel.js';
 import { CreateAnEventComponent } from './create-an-event.component';
 
 describe('CreateAnEventComponent', () => {
--- src/app/component/create-an-event/create-an-event.component.ts
+++ src/app/component/create-an-event/create-an-event.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { USERMODEL } from './userModel'; 
+import { USERMODEL } from './usermodel'; 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
